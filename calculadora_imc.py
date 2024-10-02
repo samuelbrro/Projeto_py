d601f7c03a304cbb326d8f49ebd802377d163b2c
samuelbrro <samuel.rodrigues1991@hotmail.com>
2024-10-02 00:00:00 -0300

    adicionando calculadora

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,1 +1,27 @@
 
+altura = float(input('Qual é a sua altura em cm?'))
+peso = float(input('Qual é seu peso em KG?'))
+
+resultado = peso / (altura/100)**2
+
+if resultado < 18.5:
+    print('Magreza')
+    
+elif resultado >= 18.5 and resultado < 24.9:
+    print('Normal')
+        
+elif resultado >= 25 and resultado < 29.9:
+    print('Sobrepeso')
+        
+elif resultado >= 30 and resultado < 39.9:
+    print('Obesidade')
+    
+else:
+    print('Obesidade grave!')
+    
+        
+
+        
+    
+
+
